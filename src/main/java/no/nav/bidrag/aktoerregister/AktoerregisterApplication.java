b77bc041c7564c5fa2d01d297fa371f214343f84
Jahren <brage.ekroll.jahren@nav.no>
2021-10-06 08:38:03 +0200

    Trying to make app gracefully shut down

--- src/main/java/no/nav/bidrag/aktoerregister/AktoerregisterApplication.java
+++ src/main/java/no/nav/bidrag/aktoerregister/AktoerregisterApplication.java
@@ -2,10 +2,7 @@ package no.nav.bidrag.aktoerregister;
 
 import javax.annotation.PostConstruct;
 import no.nav.bidrag.aktoerregister.jobs.TPSConsumerJob;
-import org.jobrunr.jobs.mappers.JobMapper;
 import org.jobrunr.scheduling.JobScheduler;
-import org.jobrunr.storage.InMemoryStorageProvider;
-import org.jobrunr.storage.StorageProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,8 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
+
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Bidrag Aktørregister", version = "0.2", description = "Inneholder adresse- og kontoinformasjon om aktører i Bidrassaker."))
@@ -25,15 +21,8 @@ public class AktoerregisterApplication {
     private JobScheduler jobScheduler;
 
     public static void main(String[] args) {
-        SpringApplication.run(AktoerregisterApplication.class, args);
+        System.exit(SpringApplication.exit(SpringApplication.run(AktoerregisterApplication.class, args)));
     }
-
-/*    @Bean
-    public StorageProvider storageProvider(JobMapper jobMapper) {
-        InMemoryStorageProvider storageProvider = new InMemoryStorageProvider();
-        storageProvider.setJobMapper(jobMapper);
-        return storageProvider;
-    }*/
 
     @PostConstruct
     public void startJobs() {
