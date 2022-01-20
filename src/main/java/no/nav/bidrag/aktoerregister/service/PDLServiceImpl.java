3303a6d70ea38cf66ec448898851e94118b5aa7f
Jahren <brage.ekroll.jahren@nav.no>
2022-01-20 15:21:04 +0100

    Fixed issue when error field is null

--- src/main/java/no/nav/bidrag/aktoerregister/service/PDLServiceImpl.java
+++ src/main/java/no/nav/bidrag/aktoerregister/service/PDLServiceImpl.java
@@ -2,8 +2,6 @@ package no.nav.bidrag.aktoerregister.service;
 
 import static no.nav.bidrag.aktoerregister.service.graphql.GraphQLQueryCreator.HENT_PERSON_QUERY;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import io.swagger.v3.core.util.Json;
 import java.util.Map;
 import no.nav.bidrag.aktoerregister.domene.AktoerDTO;
 import no.nav.bidrag.aktoerregister.domene.PersonDTO;
@@ -12,8 +10,6 @@ import no.nav.bidrag.aktoerregister.exception.PDLServiceException;
 import no.nav.bidrag.aktoerregister.service.graphql.GraphQLQueryCreator;
 import no.nav.bidrag.aktoerregister.service.graphql.GraphQLResponse;
 import no.nav.bidrag.commons.web.HttpHeaderRestTemplate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import no.nav.bidrag.aktoerregister.service.graphql.GraphQLQuery;
@@ -23,9 +19,6 @@ import org.springframework.web.client.HttpClientErrorException;
 public class PDLServiceImpl implements PDLService {
 
   private final HttpHeaderRestTemplate pdlRestTemplate;
-
-  private static final Logger logger = LoggerFactory.getLogger(PDLService.class);
-
 
   public PDLServiceImpl(@Qualifier("pdl") HttpHeaderRestTemplate pdlRestTemplate) {
     this.pdlRestTemplate = pdlRestTemplate;
@@ -39,32 +32,26 @@ public class PDLServiceImpl implements PDLService {
   @Override
   public PersonDTO hentRawAktoer(String id) throws PDLServiceException, AktoerNotFoundException {
     GraphQLQuery graphQLQuery = GraphQLQueryCreator.create(HENT_PERSON_QUERY, Map.of("ident", id));
-    logger.info("Query: "  + graphQLQuery.getQuery());
-    GraphQLResponse graphQLResponse = null;
+    GraphQLResponse graphQLResponse;
     try {
       graphQLResponse = pdlRestTemplate.postForEntity("/graphql", graphQLQuery, GraphQLResponse.class).getBody();
-      logger.info("Response: " + graphQLResponse);
     } catch (HttpClientErrorException e) {
-      logger.error(e.getMessage(), e);
       throw new PDLServiceException("Feil ved kall mot PDL", e);
     }
     catch (Exception e) {
-      logger.error(e.getMessage(), e);
       throw new PDLServiceException(e.getMessage(), e);
     }
     return validateResponse(graphQLResponse);
   }
 
   private PersonDTO validateResponse(GraphQLResponse graphQLResponse) throws PDLServiceException, AktoerNotFoundException {
-    logger.info("Validating response: " + graphQLResponse);
     if (graphQLResponse == null) {
       throw new PDLServiceException("Response fra PDL er null");
     }
 
-    if (!graphQLResponse.getErrors().isEmpty()) {
+    if (graphQLResponse.getErrors() != null) {
       throw new PDLServiceException(graphQLResponse.getErrors());
     }
-    logger.info("Returning data: " + graphQLResponse.getData().getHentPerson());
     return graphQLResponse.getData().getHentPerson();
   }
 }
