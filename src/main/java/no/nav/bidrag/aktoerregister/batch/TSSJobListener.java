c577d34664bb6ca500a9a316b0d6e6d3855db913
Jahren <brage.ekroll.jahren@nav.no>
2022-04-20 14:48:53 +0200

    JobListener error where execution context is empty

--- src/main/java/no/nav/bidrag/aktoerregister/batch/TSSJobListener.java
+++ src/main/java/no/nav/bidrag/aktoerregister/batch/TSSJobListener.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.annotation.AfterJob;
+import org.springframework.batch.item.ExecutionContext;
 
 public class TSSJobListener {
 
@@ -14,9 +15,18 @@ public class TSSJobListener {
   public void afterJob(JobExecution jobExecution) {
     StepExecution updateTSSAktoererStep = jobExecution.getStepExecutions().stream().filter(stepExecution -> stepExecution.getStepName().equals(TSSBatchConfig.TSS_UPDATE_AKTOERER_STEP)).findFirst().orElse(null);
     if (updateTSSAktoererStep != null) {
-      long nrTotalAktoerer = updateTSSAktoererStep.getExecutionContext().getLong(TSSBatchConfig.NR_TOTAL);
-      long nrOfUpdatedAktoerer = updateTSSAktoererStep.getExecutionContext().getLong(TSSBatchConfig.NR_UPDATED);
+      long nrTotalAktoerer = getLongFromExecutionContext(TSSBatchConfig.NR_TOTAL, updateTSSAktoererStep.getExecutionContext());
+      long nrOfUpdatedAktoerer = getLongFromExecutionContext(TSSBatchConfig.NR_UPDATED, updateTSSAktoererStep.getExecutionContext());
       logger.info("Av totalt {} aktoerer var {} blitt endret siden forrige kjøring. {} aktoerer ble dermed oppdatert i databasen.", nrTotalAktoerer, nrOfUpdatedAktoerer, nrOfUpdatedAktoerer);
+    }
+  }
+
+  private long getLongFromExecutionContext(String key, ExecutionContext executionContext) {
+    try {
+      return executionContext.getLong(key);
+    } catch (ClassCastException exception) {
+      // Value is not Long or is Null
+      return 0;
     }
   }
 }
