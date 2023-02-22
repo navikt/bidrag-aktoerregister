c4a695e331ae0e068cb080b4c132b94c2cbff19b
Simen Høston <simen.naper.hoston@webstep.no>
2023-02-22 15:01:56 +0100

    Endrer til maskinporten fremfor azure token

--- src/main/java/no/nav/bidrag/aktoerregister/api/BatchController.java
+++ src/main/java/no/nav/bidrag/aktoerregister/api/BatchController.java
@@ -4,15 +4,16 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import no.nav.bidrag.aktoerregister.batch.TSSBatchSchedulerConfig;
-import no.nav.security.token.support.core.api.Protected;
+import no.nav.security.token.support.core.api.ProtectedWithClaims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Protected
 @RestController
+@ProtectedWithClaims(
+    issuer = "maskinporten",
+    claimMap = {"scope=nav:bidrag:aktoerregister.read"})
 public class BatchController {
 
   private final TSSBatchSchedulerConfig tssBatchSchedulerConfig;
@@ -35,14 +36,6 @@ public class BatchController {
       return ResponseEntity.internalServerError()
           .body("Start av batchen feilet med følgende feilkode: " + e.getMessage());
     }
-    return ResponseEntity.ok().build();
-  }
-
-  @Operation(summary = "Henter informasjon om siste TSS batch kjøring.")
-  @ApiResponse(responseCode = "200", description = "Hentet informasjon om siste TSS batch kjøring.")
-  @GetMapping(path = "/tssBatch")
-  public ResponseEntity<?> hentTssBatchInfo() {
-
     return ResponseEntity.ok().build();
   }
 }
