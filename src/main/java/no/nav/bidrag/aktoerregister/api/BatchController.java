324fe964388e3a3540ee1ad669ec104164be8aed
Simen Høston <simen.naper.hoston@webstep.no>
2023-02-23 08:47:16 +0100

    Endrer manuell kjøring av batchen til å være async for å ikke stå å vente på kallet

--- src/main/java/no/nav/bidrag/aktoerregister/api/BatchController.java
+++ src/main/java/no/nav/bidrag/aktoerregister/api/BatchController.java
@@ -2,9 +2,11 @@ package no.nav.bidrag.aktoerregister.api;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import java.util.concurrent.CompletableFuture;
 import no.nav.bidrag.aktoerregister.batch.TSSBatchSchedulerConfig;
 import no.nav.security.token.support.core.api.ProtectedWithClaims;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,25 +19,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class BatchController {
 
   private final TSSBatchSchedulerConfig tssBatchSchedulerConfig;
+  private final Logger LOGGER = LoggerFactory.getLogger(BatchController.class);
 
   @Autowired
   public BatchController(TSSBatchSchedulerConfig tssBatchSchedulerConfig) {
     this.tssBatchSchedulerConfig = tssBatchSchedulerConfig;
   }
 
-  @Operation(summary = "Start kjøring av TSS batch.")
-  @ApiResponses({
-    @ApiResponse(responseCode = "200", description = "TSS batchen ble startet."),
-    @ApiResponse(responseCode = "500", description = "Noe gikk galt under start av batchen.")
-  })
+  @Operation(summary = "Start kjøring av TSS batch.",
+      description = "TSS batchen startes asynkront. Dette vil medføre at feil under kjøring av batchen ikke vil reflekteres i responskoden dette endepunktet returnerer.")
+  @ApiResponse(responseCode = "200", description = "TSS batchen ble startet.")
   @PostMapping(path = "/tssBatch")
   public ResponseEntity<?> startTssBatch() {
-    try {
-      tssBatchSchedulerConfig.scheduleTSSBatch();
-    } catch (Exception e) {
-      return ResponseEntity.internalServerError()
-          .body("Start av batchen feilet med følgende feilkode: " + e.getMessage());
-    }
+    CompletableFuture.runAsync(
+        () -> {
+          try {
+            tssBatchSchedulerConfig.scheduleTSSBatch();
+          } catch (Exception e) {
+            LOGGER.error(
+                "Manuell start av batchen feilet med følgende feilkode: {}", e.getMessage());
+          }
+        });
     return ResponseEntity.ok().build();
   }
 }
