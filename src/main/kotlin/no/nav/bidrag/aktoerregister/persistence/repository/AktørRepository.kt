8d2b1a80332518ace3fcba01838cd5592f4039e7
Simen Høston <simen.naper.hoston@webstep.no>
2023-05-22 14:12:54 +0200

    Legger tilbake metode nødvendig for batch

--- src/main/kotlin/no/nav/bidrag/aktoerregister/persistence/repository/AktørRepository.kt
+++ src/main/kotlin/no/nav/bidrag/aktoerregister/persistence/repository/AktørRepository.kt
@@ -1,9 +1,15 @@
 package no.nav.bidrag.aktoerregister.persistence.repository
 
 import no.nav.bidrag.aktoerregister.persistence.entities.Aktør
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface AktørRepository : JpaRepository<Aktør, String> {
+
+    // Brukes av batchReader
+    @Suppress("unused")
+    fun findAllByAktørType(aktørType: String, pageable: Pageable): Page<Aktør>
 
     fun findByAktørIdent(aktørIdent: String): Aktør?
 
