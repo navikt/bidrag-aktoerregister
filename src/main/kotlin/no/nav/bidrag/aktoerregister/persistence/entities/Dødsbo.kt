5e96ca4d376883d1db4f40d20353b73ad2a7360c
Bent J. Lorentzen <bent.lorentzen@nav.no>
2023-06-07 14:21:56 +0200

    Spring Boot 3

--- src/main/kotlin/no/nav/bidrag/aktoerregister/persistence/entities/Dødsbo.kt
+++ src/main/kotlin/no/nav/bidrag/aktoerregister/persistence/entities/Dødsbo.kt
@@ -1,12 +1,12 @@
 package no.nav.bidrag.aktoerregister.persistence.entities
 
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
-import javax.persistence.JoinColumn
-import javax.persistence.OneToOne
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToOne
 
 @Entity(name = "dodsbo")
 data class Dødsbo(
