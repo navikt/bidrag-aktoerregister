a7db5dce2b2e2671b20eb409d88c2407b2325a64
Simen Høston <simen.naper.hoston@webstep.no>
2023-08-07 14:40:37 +0200

    Hopper over hendelser hvor aktør ikke finnes i bidrag-person

--- src/main/kotlin/no/nav/bidrag/aktoerregister/service/PersonHendelseService.kt
+++ src/main/kotlin/no/nav/bidrag/aktoerregister/service/PersonHendelseService.kt
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.transaction.Transactional
 import no.nav.bidrag.aktoerregister.SECURE_LOGGER
 import no.nav.bidrag.aktoerregister.dto.Endringsmelding
+import no.nav.bidrag.aktoerregister.exception.AktørNotFoundException
 import no.nav.bidrag.domain.ident.Ident
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
@@ -12,6 +14,10 @@ class PersonHendelseService(
     private val objectMapper: ObjectMapper,
     private val aktørService: AktørService
 ) {
+
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(PersonHendelseService::class.java)
+    }
 
     @Transactional
     fun behandleHendelse(hendelse: String) {
@@ -22,10 +28,15 @@ class PersonHendelseService(
             val aktør = aktørService.hentAktørFraDatabase(Ident(ident))
             aktør?.let {
                 SECURE_LOGGER.info("Fant lagret aktør $it. Oppdaterer med nye verdier.")
-                val aktørFraPerson = aktørService.hentAktørFraPerson(Ident(ident))
-                aktør.oppdaterAlleFelter(aktørFraPerson)
-                aktørService.lagreEllerOppdaterAktør(aktør, ident)
-                return
+                try {
+                    val aktørFraPerson = aktørService.hentAktørFraPerson(Ident(ident))
+                    aktør.oppdaterAlleFelter(aktørFraPerson)
+                    aktørService.lagreEllerOppdaterAktør(aktør, ident)
+                    return
+                } catch (e: AktørNotFoundException) {
+                    LOGGER.error("Aktør ikke funnet i bidrag-person! Se sikker logg for mer info.")
+                    SECURE_LOGGER.error("Aktør ikke funnet i bidrag-person! Fant ikke person for hendelse: $hendelse")
+                }
             }
         }
     }
