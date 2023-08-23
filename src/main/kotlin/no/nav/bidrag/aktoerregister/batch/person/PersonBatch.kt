c08419656611d41cb326cb82ff7af538ee71f055
Simen Høston <simen.naper.hoston@webstep.no>
2023-08-23 10:26:09 +0200

    Revert "Skriver om restart av personBatch"
    This reverts commit f3151ae95e1e1c8a19f981dfdb8281722fbeb8f4.

--- src/main/kotlin/no/nav/bidrag/aktoerregister/batch/person/PersonBatch.kt
+++ src/main/kotlin/no/nav/bidrag/aktoerregister/batch/person/PersonBatch.kt
@@ -1,13 +1,12 @@
 package no.nav.bidrag.aktoerregister.batch.person
 
-import no.nav.bidrag.aktoerregister.batch.person.PersonBatchConfig.Companion.PERSON_BATCH_OPPDATERING_JOB
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.JobParametersBuilder
 import org.springframework.batch.core.launch.JobLauncher
 import org.springframework.batch.core.launch.JobOperator
 import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
+import java.text.SimpleDateFormat
 import java.util.*
 
 @Component
@@ -19,19 +18,12 @@ class PersonBatch(
 
     fun startPersonBatch() {
         val jobParameters = JobParametersBuilder()
-                .addString("jobIdentifier", "RESTARTABLE_JOB")
+                .addString("time", SimpleDateFormat("yyyy-MM-dd HH:mm").format(Calendar.getInstance().time))
                 .toJobParameters()
         jobLauncher.run(job, jobParameters)
     }
 
-    fun restartPersonBatch(): ResponseEntity<*> {
-        val jobExecutionIds = jobOperator.getRunningExecutions(PERSON_BATCH_OPPDATERING_JOB)
-        return if (jobExecutionIds.isNotEmpty()) {
-            val latestExecutionId = jobExecutionIds.maxOrNull() ?: throw IllegalStateException("No running executions found.")
-            jobOperator.restart(latestExecutionId)
-            ResponseEntity.ok().build<Any>()
-        } else {
-            ResponseEntity.notFound().build<Any>()
-        }
+    fun restartPersonBatch(executionId: Long) {
+        jobOperator.restart(executionId)
     }
 }
