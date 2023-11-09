f6252b49e89258d49d796c3fa8fbb4c389e8781a
Simen Høston <simen.naper.hoston@webstep.no>
2023-11-09 17:26:05 +0100

    Legger til nano for utehenting av sistEndret tidspunkt

--- src/main/kotlin/no/nav/bidrag/aktoerregister/persistence/repository/AktørRepository.kt
+++ src/main/kotlin/no/nav/bidrag/aktoerregister/persistence/repository/AktørRepository.kt
@@ -4,7 +4,6 @@ import no.nav.bidrag.aktoerregister.persistence.entities.Aktør
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import java.sql.Timestamp
 
 interface AktørRepository : JpaRepository<Aktør, String> {
