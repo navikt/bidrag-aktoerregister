d729466eb088ebb21d63a2b24d9ca1b9bbcee4bf
Simen Høston <simen.naper.hoston@webstep.no>
2023-11-10 13:27:39 +0100

    Fjerner spesiell batchkjøringsparameter

--- src/main/kotlin/no/nav/bidrag/aktoerregister/persistence/repository/AktørRepository.kt
+++ src/main/kotlin/no/nav/bidrag/aktoerregister/persistence/repository/AktørRepository.kt
@@ -4,24 +4,12 @@ import no.nav.bidrag.aktoerregister.persistence.entities.Aktør
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
-import java.sql.Timestamp
 
 interface AktørRepository : JpaRepository<Aktør, String> {
 
     // Brukes av batchReader
     @Suppress("unused")
     fun findAllByAktørType(aktørType: String, pageable: Pageable): Page<Aktør>
-
-    @Suppress("unused")
-    @Query(
-        nativeQuery = true,
-        value = "SELECT * FROM aktoerregister.aktoer WHERE aktoertype = ?1 AND sist_endret < ?2",
-    )
-    fun finnAlleIkkeKjørte(aktørType: String, sistEndret: Timestamp, pageable: Pageable): Page<Aktør>
-
-    @Suppress("unused")
-    fun findAllBySistEndretLessThan(sistEndretTimestamp: Timestamp, pageable: Pageable): Page<Aktør>
 
     fun findByAktørIdent(aktørIdent: String): Aktør?
 
