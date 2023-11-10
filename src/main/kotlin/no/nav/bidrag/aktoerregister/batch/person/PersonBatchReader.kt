b6f0706d5afd36d074b81b821e2e174efc217611
Simen Høston <simen.naper.hoston@webstep.no>
2023-11-10 13:27:39 +0100

    Fjerner spesiell batchkjøringsparameter

--- src/main/kotlin/no/nav/bidrag/aktoerregister/batch/person/PersonBatchReader.kt
+++ src/main/kotlin/no/nav/bidrag/aktoerregister/batch/person/PersonBatchReader.kt
@@ -1,21 +1,20 @@
 package no.nav.bidrag.aktoerregister.batch.person
 
+import no.nav.bidrag.aktoerregister.dto.enumer.Identtype
 import no.nav.bidrag.aktoerregister.persistence.entities.Aktør
 import no.nav.bidrag.aktoerregister.persistence.repository.AktørRepository
 import org.springframework.batch.item.ItemReader
 import org.springframework.batch.item.data.RepositoryItemReader
 import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Component
-import java.sql.Timestamp
-import java.time.LocalDateTime
 import java.util.Collections
 
 @Component
 class PersonBatchReader(aktoerRepository: AktørRepository) : RepositoryItemReader<Aktør>(), ItemReader<Aktør> {
     init {
         this.setRepository(aktoerRepository)
-        this.setMethodName("findAllBySistEndretLessThan")
-        this.setArguments(listOf(Timestamp.valueOf(LocalDateTime.of(2023, 11, 8, 13, 20, 0))))
+        this.setMethodName("findAllByAktørType")
+        this.setArguments(listOf(Identtype.PERSONNUMMER.name))
         this.setPageSize(100)
         this.setSort(Collections.singletonMap("aktørIdent", Sort.Direction.ASC))
     }
