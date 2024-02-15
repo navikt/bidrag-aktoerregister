4e176964160ce0779cec73acb805285fd6ffcb78
Simen Høston <simen.naper.hoston@webstep.no>
2024-02-15 16:09:43 +0100

    Bugfix på person batch som førte til skip på writes

--- src/main/kotlin/no/nav/bidrag/aktoerregister/batch/person/PersonBatchProcessor.kt
+++ src/main/kotlin/no/nav/bidrag/aktoerregister/batch/person/PersonBatchProcessor.kt
@@ -4,7 +4,6 @@ import io.github.oshai.kotlinlogging.KotlinLogging
 import no.nav.bidrag.aktoerregister.SECURE_LOGGER
 import no.nav.bidrag.aktoerregister.batch.AktørBatchProcessorResult
 import no.nav.bidrag.aktoerregister.batch.AktørStatus
-import no.nav.bidrag.aktoerregister.dto.enumer.Identtype
 import no.nav.bidrag.aktoerregister.persistence.entities.Aktør
 import no.nav.bidrag.aktoerregister.service.AktørService
 import no.nav.bidrag.domene.ident.Ident
@@ -20,15 +19,12 @@ class PersonBatchProcessor(
 
     override fun process(aktør: Aktør): AktørBatchProcessorResult? {
         return try {
-            if (aktør.aktørType == Identtype.PERSONNUMMER.name) {
-                aktørService.hentAktørFraPerson(Ident(aktør.aktørIdent))
-                    .takeIf { it != aktør }
-                    ?.let {
-                        val originalIdent = if (it.aktørIdent != aktør.aktørIdent) aktør.aktørIdent else null
-                        AktørBatchProcessorResult(aktør, it, AktørStatus.UPDATED, originalIdent)
-                    }
-            }
-            null
+            aktørService.hentAktørFraPerson(Ident(aktør.aktørIdent))
+                .takeIf { it != aktør }
+                ?.let {
+                    val originalIdent = if (it.aktørIdent != aktør.aktørIdent) aktør.aktørIdent else null
+                    AktørBatchProcessorResult(aktør, it, AktørStatus.UPDATED, originalIdent)
+                }
         } catch (e: Exception) {
             SECURE_LOGGER.error("Person: ${aktør.aktørIdent} feilet i PersonBatchProcessor. Feilmelding: ${e.message}")
             LOGGER.error(e) { e.message }
